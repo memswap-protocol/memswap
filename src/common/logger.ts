2dfe7f493f4fb6004429d0dae835e60b00e9641a
George Roman <george.roman.99@gmail.com>
2023-08-28 15:17:46 +0200

    refactor: logger

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,34 +1,32 @@
 import { createLogger, format, transports } from "winston";
 
-const log = (level: "error" | "info" | "warn") => {
-  const service = "blur-mirror";
+const service = process.env.SERVICE;
+const internalLogger = createLogger({
+  exitOnError: false,
+  format: format.combine(
+    format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    format.json()
+  ),
+  transports: [
+    process.env.DATADOG_API_KEY
+      ? new transports.Http({
+          host: "http-intake.logs.datadoghq.com",
+          path: `/api/v2/logs?dd-api-key=${process.env.DATADOG_API_KEY}&ddsource=nodejs&service=${service}`,
+          ssl: true,
+        })
+      : // Fallback to logging to standard output
+        new transports.Console(),
+  ],
+});
 
-  const logger = createLogger({
-    exitOnError: false,
-    format: format.combine(
-      format.timestamp({
-        format: "YYYY-MM-DD HH:mm:ss",
-      }),
-      format.json()
-    ),
-    transports: [
-      process.env.DATADOG_API_KEY
-        ? new transports.Http({
-            host: "http-intake.logs.datadoghq.com",
-            path: `/api/v2/logs?dd-api-key=${process.env.DATADOG_API_KEY}&ddsource=nodejs&service=${service}`,
-            ssl: true,
-          })
-        : // Fallback to logging to standard output
-          new transports.Console(),
-    ],
-  });
-
-  return (component: string, message: string) =>
-    logger.log(level, message, {
+const log =
+  (level: "error" | "info" | "warn") => (component: string, message: string) =>
+    internalLogger.log(level, message, {
       component,
       version: process.env.npm_package_version,
     });
-};
 
 export const logger = {
   error: log("error"),
